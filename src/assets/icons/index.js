efc6fac0763ee6e8f9d46d03293b014e17a10914
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2024-03-17 14:15:00 +0100

    feat: add a base set of social icons and configure a project level prettierrc file

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,37 @@
-export { default as Arrow } from "@assets/icons/Arrow.astro";
-export { default as ArrowRounded } from "@assets/icons/ArrowRounded.astro";
-export { default as Chevron } from "@assets/icons/Chevron.astro";
-export { default as ChevronRounded } from "@assets/icons/ChevronRounded.astro";
+// ===========================================================================
+// Arrow icons
+// ===========================================================================
+export { default as Arrow } from "@assets/icons/Arrow.astro"
+export { default as ArrowRounded } from "@assets/icons/ArrowRounded.astro"
+export { default as Chevron } from "@assets/icons/Chevron.astro"
+export { default as ChevronRounded } from "@assets/icons/ChevronRounded.astro"
+export { default as CircleArrow } from "@assets/icons/CircleArrow.astro"
+export { default as CircleArrowRounded } from "@assets/icons/CircleArrowRounded.astro"
+export { default as DiagonalArrow } from "@assets/icons/DiagonalArrow.astro"
+export { default as DiagonalArrowRounded } from "@assets/icons/DiagonalArrowRounded.astro"
+
+// ===========================================================================
+// Social icons
+// ===========================================================================
+export { default as Discord } from "@assets/icons/Discord.astro"
+export { default as Facebook } from "@assets/icons/Facebook.astro"
+export { default as FrontendMentor } from "@assets/icons/FrontendMentor.astro"
+export { default as Github } from "@assets/icons/Github.astro"
+export { default as Instagram } from "@assets/icons/Instagram.astro"
+export { default as Linkedin } from "@assets/icons/Linkedin.astro"
+export { default as Messenger } from "@assets/icons/Messenger.astro"
+export { default as Pinterest } from "@assets/icons/Pinterest.astro"
+export { default as Reddit } from "@assets/icons/Reddit.astro"
+export { default as Snapchat } from "@assets/icons/Snapchat.astro"
+export { default as Spotify } from "@assets/icons/Spotify.astro"
+export { default as Twitch } from "@assets/icons/Twitch.astro"
+export { default as Whatsapp } from "@assets/icons/Whatsapp.astro"
+export { default as X } from "@assets/icons/X.astro"
+export { default as Youtube } from "@assets/icons/Youtube.astro"
+
+// ===========================================================================
+// Social icons
+// ===========================================================================
+export { default as Mail } from "@assets/icons/Mail.astro"
+export { default as Phone } from "@assets/icons/Phone.astro"
+export { default as Smartphone } from "@assets/icons/Smartphone.astro"
