6260a13853e039d589fe5b7a347c83bf03950a73
BramMortier <bram.mortier92@gmail.com>
2024-03-18 16:40:55 +0100

    feat: add a cards block

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "@blocks/Header.astro"
 export { default as Footer } from "@blocks/Footer.astro"
+export { default as Cards } from "@blocks/Cards.astro"
---
+++ src/config/pages/homepage.js
@@ -0,0 +1,31 @@
+import { example5_4 } from "@assets/images/placeholders/index"
+
+export const content = {
+	cards: {
+		title: "These are some cards",
+		text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo quo nihil aliquid. Cumque, suscipit impedit blanditiis laboriosam quasi distinctio! Nulla quis veritatis fugiat aut eveniet cupiditate iste quaerat natus dicta?",
+		items: [
+			{
+				image: example5_4,
+				text: "Nesciunt quo illum possimus commodi beatae quam exercitationem! Voluptate voluptatibus aliquam alias.",
+				button: {
+					text: "View options"
+				}
+			},
+			{
+				image: example5_4,
+				text: "Nesciunt quo illum possimus commodi beatae quam exercitationem! Voluptate voluptatibus aliquam alias.",
+				button: {
+					text: "View options"
+				}
+			},
+			{
+				image: example5_4,
+				text: "Nesciunt quo illum possimus commodi beatae quam exercitationem! Voluptate voluptatibus aliquam alias.",
+				button: {
+					text: "View options"
+				}
+			}
+		]
+	}
+}
