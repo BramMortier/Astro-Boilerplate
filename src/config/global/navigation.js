ce85289c6e5754ae91523962c8f06019e6b3190c
BramMortier <bram.mortier92@gmail.com>
2024-03-18 10:20:37 +0100

    feat: add the config for nav and social stuff

---
+++ src/config/global/navigation.js
@@ -0,0 +1,20 @@
+export const navigation = {
+    links: [
+        {
+            text: "Home",
+            href: "#",
+        },
+        {
+            text: "About",
+            href: "#",
+        },
+        {
+            text: "Blog",
+            href: "#",
+        },
+        {
+            text: "Contact",
+            href: "#",
+        },
+    ]
+}
