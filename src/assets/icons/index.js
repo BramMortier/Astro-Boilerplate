07ae738e58bbd2fbbd8ff40c576075abff5132da
BramMortier <bram.mortier92@gmail.com>
2024-03-19 17:24:16 +0100

    feat: init project

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,37 +1,18 @@
 // =============================================================================
-// Arrow icons
+// Icons
 // =============================================================================
 export { default as Arrow } from "@assets/icons/Arrow.astro"
-export { default as ArrowRounded } from "@assets/icons/ArrowRounded.astro"
-export { default as Chevron } from "@assets/icons/Chevron.astro"
-export { default as ChevronRounded } from "@assets/icons/ChevronRounded.astro"
-export { default as CircleArrow } from "@assets/icons/CircleArrow.astro"
-export { default as CircleArrowRounded } from "@assets/icons/CircleArrowRounded.astro"
-export { default as DiagonalArrow } from "@assets/icons/DiagonalArrow.astro"
-export { default as DiagonalArrowRounded } from "@assets/icons/DiagonalArrowRounded.astro"
-
-// =============================================================================
-// Social icons
-// =============================================================================
-export { default as Discord } from "@assets/icons/Discord.astro"
+export { default as BgDots } from "@assets/icons/BgDots.astro"
+export { default as Bookmark } from "@assets/icons/Bookmark.astro"
+export { default as Chrome } from "@assets/icons/Chrome.astro"
+export { default as Close } from "@assets/icons/Close.astro"
+export { default as Error } from "@assets/icons/Error.astro"
 export { default as Facebook } from "@assets/icons/Facebook.astro"
-export { default as FrontendMentor } from "@assets/icons/FrontendMentor.astro"
-export { default as Github } from "@assets/icons/Github.astro"
-export { default as Instagram } from "@assets/icons/Instagram.astro"
-export { default as Linkedin } from "@assets/icons/Linkedin.astro"
-export { default as Messenger } from "@assets/icons/Messenger.astro"
-export { default as Pinterest } from "@assets/icons/Pinterest.astro"
-export { default as Reddit } from "@assets/icons/Reddit.astro"
-export { default as Snapchat } from "@assets/icons/Snapchat.astro"
-export { default as Spotify } from "@assets/icons/Spotify.astro"
-export { default as Twitch } from "@assets/icons/Twitch.astro"
-export { default as Whatsapp } from "@assets/icons/Whatsapp.astro"
-export { default as X } from "@assets/icons/X.astro"
-export { default as Youtube } from "@assets/icons/Youtube.astro"
-
-// =============================================================================
-// Social icons
-// =============================================================================
-export { default as Mail } from "@assets/icons/Mail.astro"
-export { default as Phone } from "@assets/icons/Phone.astro"
-export { default as Smartphone } from "@assets/icons/Smartphone.astro"
+export { default as Firefox } from "@assets/icons/Firefox.astro"
+export { default as Hamburger } from "@assets/icons/Hamburger.astro"
+export { default as Opera } from "@assets/icons/Opera.astro"
+export { default as Twitter } from "@assets/icons/Twitter.astro"
+export { default as HeroIllustration } from "@assets/icons/HeroIllustration.astro"
+export { default as OneClickBookmark } from "@assets/icons/OneClickBookmark.astro"
+export { default as ShareBookmarks } from "@assets/icons/ShareBookmarks.astro"
+export { default as IntelligentSearch } from "@assets/icons/IntelligentSearch.astro"
